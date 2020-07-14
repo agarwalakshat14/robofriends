bc3713dd682d5fd48f206059ed44974c2864320b
Akshat Agarwal <33230795+agarwalakshat14@users.noreply.github.com>
2020-07-14 17:34:26 +0530

    Add files via upload

---
+++ Robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+	{
+		id:1,
+		name:'Akshat Agarwal',
+		username:'14k',
+		email:'agarwalakshat14k@gmail.com'
+	},
+	{
+		id:2,
+		name:'Akshika Agarwal',
+		username:'Pragya',
+		email:'agarwalakshika14k@gmail.com'
+	},
+	{
+		id:3,
+		name:'Yadu Agarwal',
+		username:'Padu',
+		email:'agarwalyadu14k@gmail.com'
+	},
+	{
+		id:4,
+		name:'Kavisha Agarwal',
+		username:'Golu',
+		email:'agarwalkavisha4k@gmail.com'
+	},
+	{
+		id:5,
+		name:'Akash Agarwal',
+		username:'Tinku',
+		email:'agarwalakash14k@gmail.com'
+	},
+	{
+		id:6,
+		name:'Anita Goel',
+		username:'Abha',
+		email:'agarwalanita14k@gmail.com'
+	},
+	{
+		id:7,
+		name:'Amita Goel',
+		username:'Priti',
+		email:'agarwalamita14k@gmail.com'
+	},
+	{
+		id:8,
+		name:'Alpna Goel',
+		username:'Raju',
+		email:'agarwalalpna14k@gmail.com'
+	},
+	{
+		id:9,
+		name:'Anuradha Goel',
+		username:'Deepa',
+		email:'agarwalanuradha14k@gmail.com'
+	},
+	{
+		id:10,
+		name:'Sachin Goel',
+		username:'Sonu',
+		email:'agarwalsachin14k@gmail.com'
+	},
+];
